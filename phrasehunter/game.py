b7c7b6b88fa61e6f10aed2da1e12d461812976af
d0dders <dodders@outlook.com>
2020-11-02 21:36:40 +0000

    Completed game logic

--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -3,22 +3,33 @@ import random
 
 class Game:
 
+    STARTING_LIVES = 5
+
     def __init__(self):
         self.missed = 0
         self.phrases = [Phrase('A New Hope'),
                         Phrase('The Empire Strikes Back'),
                         Phrase('Return of the Jedi'),
                         Phrase('The Force Awakens'),
-                        Phrase('The Last of the Jedi')]
+                        Phrase('The Last Jedi')]
         self.active_phrase = None
-        self.guesses = []
+        #guesses contains space to simply win checking
+        self.guesses = [' ']
     
     def start(self):
         self.welcome()
         self.active_phrase = self.get_random_phrase()
-        print(self.active_phrase)
-        #get_guess()
-        #process guess
+        #print(self.active_phrase)
+        still_playing = True
+        while still_playing:
+            self.active_phrase.display(self.guesses)
+            self.get_guess()
+            if self.active_phrase.check_complete(self.guesses) == True:
+                still_playing = False
+                self.game_over('win')
+            elif self.missed >= self.STARTING_LIVES:
+                still_playing = False
+                self.game_over('lose')
 
     def get_random_phrase(self):
         return self.phrases[random.randint(0, 4)]
@@ -26,4 +37,24 @@ class Game:
     def welcome(self):
         """ Prints a short welcome message to console        
         """
-        print("Welcome to PHRASEHUNTER")
+        print("""
+  ___| |_ __ _ _ __  __      ____ _ _ __ ___ 
+ / __| __/ _` | '__| \ \ /\ / / _` | '__/ __|
+ \__ \ || (_| | |     \ V  V / (_| | |  \__ \\
+ |___/\__\__,_|_|      \_/\_/ \__,_|_|  |___/
+        """)
+        print("           Welcome to PHRASEHUNTER")
+        print("\nGuess the Star Wars movie title...")
+
+    def get_guess(self):
+        guess = input("\nGuess a letter:  ")
+        self.guesses.append(guess)
+        if not self.active_phrase.check_letter(guess):
+            self.missed += 1
+            print(f"\nYou have {self.STARTING_LIVES - self.missed} out of {self.STARTING_LIVES} lives remaining!")
+        
+    def game_over(self, win_or_lose):
+        if win_or_lose == 'win':
+            print("\nCongratulations, you won! The force is strong in you!\n")
+        else:
+            print("\nLost you did. More trainng you will need.\n")
--- phrasehunter/phrase.py
+++ phrasehunter/phrase.py
@@ -6,13 +6,28 @@ class Phrase:
     def __str__(self):
         return self.phrase
     
-    def display():
-        """Displays phrase to console showing corrctly guessed letters
+    def display(self, guesses):
+        """Takes a list of the guessed letters.
+        Displays phrase to console showing corrctly guessed letters
         """
-        pass
+        display_string_list = []
+        for character in self.phrase:
+            if character == ' ':
+                display_string_list.append(character)
+            elif character >= 'a' and character <= 'z':
+                if character in guesses:
+                    display_string_list.append(character)
+                else:
+                    display_string_list.append('_')
+        display_string = ' '.join(display_string_list)
+        print("\n" + display_string)
 
-    def check_letter():
-        pass
+    def check_letter(self, letter):
+        if letter in self.phrase:
+            return True
 
-    def check_complete():
-        pass
+    def check_complete(self, guesses):
+        for letter in self.phrase:
+            if letter not in guesses:
+                return False
+        return True
